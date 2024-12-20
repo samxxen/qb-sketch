c35eec1adb08eb00123d9ad7f89ad4339d413f37
sam xxen <samxxen@gmail.com>
2024-12-20 22:03:16 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,25 @@
+# codescript
+Sketch and Send is a highly-configurable FiveM plugin that allows users to communicate through sketching drawings and sending them to fellow members
+with the option to use ACE permissions to enhance security.
+
+Copyright © 2024 codescript - All rights reservered, MIT license 
+
+## Prerequisites
+- [imgbb API key](https://api.imgbb.com)
+
+
+
+
+## ImgBB Setup
+- Create an account with ImgBB
+- Goto https://api.imgbb.com/
+- Click "Get API key"
+- Copy your API key into the value of `Config.imgbbApiKey`
+- Go to your profile
+- Set your profile to private (in case of NSFW content being drawn)
+
+### ImgBB Note
+ImgBB may rate limit you and return status code 400 if too many sketches are made at once, in that case please just wait patiently in order for everything to
+return to normal. I highly recommend restricting sketches to certain ACE groups to avoid this.
+
+
---
+++ server.lua
@@ -0,0 +1,143 @@
+
+
+--[[
+    DISCORD REFERENCES
+
+    sendMessage
+    sendMessage(ID, Content)
+
+    sendEmbed
+    local embedFields = {
+        { name = "Field 1", value = "Value 1" },
+        { name = "Field 2", value = "Value 2" }
+    }
+    sendEmbed(ID, Title, Description, Color, embedFields)
+]]--
+
+
+-- Command handling
+
+RegisterCommand(Config.sketchCommand, function(source, args, rawCommand)
+    if Config.limitACE_Sketch then
+        if IsAceAllowed(source, 'sketchnsend.' .. Config.sketchPermission) then
+            TriggerClientEvent('SketchNSend:ShowSketchpad', source)
+        end
+    else
+        TriggerClientEvent('SketchNSend:ShowSketchpad', source)
+    end
+end)
+
+-- Event Handling
+
+-- Save codescript Request
+-- Limits viewing sketches made by players, including requests being made to those players
+RegisterNetEvent('SketchNSend:SaveSketch')
+AddEventHandler('SketchNSend:SaveSketch', function(source, data)
+    function saveSketch_fn(success, url)
+        if success then
+            local eventData = {
+                senderId = source,
+                requestId = data.requestId,
+                url = url,
+                timeout = os.time() + Config.requestTimeout
+            }
+
+            if Config.limitACE_View then
+                if IsAceAllowed(data.requestId, 'sketchnsend.' .. Config.viewPermission) then
+                    TriggerClientEvent('SketchNSend:RequestChange', data.requestId, eventData)
+                    print('New sketch request from ' .. GetPlayerName(source) .. ' (Server #' .. source .. ')' .. ' to ' .. GetPlayerName(data.requestId) .. ' (Server #' .. source .. ')')
+                end
+            else
+                TriggerClientEvent('SketchNSend:RequestChange', data.requestId, eventData)
+                print('New sketch request from ' .. GetPlayerName(source) .. ' (Server #' .. source .. ')' .. ' to ' .. GetPlayerName(data.requestId) .. ' (Server #' .. source .. ')')
+            end
+        else
+            print('An error has occured when uploading image to ImgBB. Do you have an API key configured?')
+        end
+    end
+
+    if Config.limitACE_Sketch then
+        if IsAceAllowed(source, 'sketchnsend.' .. Config.sketchPermission) then
+            saveSketch(data.image, saveSketch_fn)
+        else
+            print('Player ID: ' .. tostring(source) .. ' does not have permission to save sketches.')
+        end
+    else
+        saveSketch(data.image, saveSketch_fn)
+    end
+end)
+
+RegisterNetEvent('SketchNSend:CheckTimeout')
+AddEventHandler('SketchNSend:CheckTimeout', function(clientTimeout)
+    TriggerClientEvent('SketchNSend:HandleTimeout', source, os.time(), clientTimeout)
+end)
+
+RegisterNetEvent('SketchNSend:GetPlayerData_View')
+AddEventHandler('SketchNSend:GetPlayerData_View', function(senderId, url)
+    local playerInfo = {
+        senderName = GetPlayerName(senderId),
+        senderId = senderId
+    }
+
+    TriggerClientEvent('SketchNSend:PlayerDataReceived_View', senderId, playerInfo, url)
+end)
+
+RegisterNetEvent('SketchNSend:GetPlayerList')
+AddEventHandler('SketchNSend:GetPlayerList', function()
+    local players = {}
+    for _, id in ipairs(GetPlayers()) do
+        table.insert(players, {
+            id = id,
+            name = GetPlayerName(id)
+        })
+    end
+
+    TriggerClientEvent('SketchNSend:RecievePlayerList', source, players)
+end)
+
+RegisterNetEvent('SketchNSend:RequestResponse')
+AddEventHandler('SketchNSend:RequestResponse', function(senderId, accepted)
+    if accepted then
+        TriggerClientEvent('chat:addMessage',
+            senderId,
+            {
+                color = { 0, 255, 0 },
+                multiline = true,
+                args = { '[SketchNSend]', string.format('Your sketch view request was accepted.') }
+            }
+        )
+    else
+        TriggerClientEvent('chat:addMessage',
+            senderId,
+            {
+                color = { 255, 0, 0 },
+                multiline = true,
+                args = { '[SketchNSend]', string.format('Your sketch view request was denied.') }
+            }
+        )
+    end
+end)
+
+RegisterNetEvent('SketchNSend:LogToDiscord')
+AddEventHandler('SketchNSend:LogToDiscord', function(url, senderId, requestId)
+    local senderName = GetPlayerName(senderId)
+    local identifiers = GetPlayerIdentifiers(senderId)
+    local timestamp = os.date('%Y-%m-%d %H:%M:%S', os.time())
+
+    local formattedIdentifiers = {}
+    for _, id in ipairs(identifiers) do
+        table.insert(formattedIdentifiers, '* ' .. id)
+    end
+
+    local identifiersString = table.concat(formattedIdentifiers, '\n')
+
+    local requestName = GetPlayerName(requestId)
+
+    local embedFields = {
+        { name = "From", value = senderName .. '(Server #' .. senderId .. ')', inline = true },
+        { name = "To", value = requestName .. '(Server #' .. requestId .. ')', inline = true },
+        { name = "Sender Identifiers", value = identifiersString, inline = false }
+    }
+
+    sendEmbed(Config.discordChannelId, "Sketch Request", "A new sketch request has been logged.", 3447003, embedFields, url)
+end)
